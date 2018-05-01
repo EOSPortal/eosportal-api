00137c320fca8d966648919e03d7a45de5ef88cf
zaratustra <zrts@protonmail.com>
2018-05-01 09:05:39 -0300

    create routes

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,35 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('blocks/all', [
+    'as' => 'blocks.all',
+    'uses' => 'BlocksController@all'
+]);
+Route::get('blocks', [
+    'as' => 'blocks',
+    'uses' => 'BlocksController@index'
+]);
+Route::get('chains/all', [
+    'as' => 'chains.all',
+    'uses' => 'ChainsController@all'
+]);
+Route::get('chains', [
+    'as' => 'chains',
+    'uses' => 'ChainsController@index'
+]);
+Route::get('info/all', [
+    'as' => 'info.all',
+    'uses' => 'InfoController@all'
+]);
+Route::get('info', [
+    'as' => 'info',
+    'uses' => 'InfoController@index'
+]);
+Route::get('producers/all', [
+    'as' => 'producers.all',
+    'uses' => 'ProducersController@all'
+]);
+Route::get('producers', [
+    'as' => 'producers',
+    'uses' => 'ProducersController@index'
+]);
