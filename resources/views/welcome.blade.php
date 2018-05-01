ded29aff20ebd853511c57d06eed6573a5cadde2
zaratustra <zrts@protonmail.com>
2018-05-01 12:13:02 +0200

    small change to sample page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -98,22 +98,21 @@
     <div class="container">
 
         <h1 class="text-center p-4 m-4">Testnets</h1>
-
         <div class="d-flex justify-content-center">
-            <div class="card w-100 m-2">
-                <div class="card-body">
-                    <h5 class="card-title">Jungle</h5>
-                    <h6 class="card-subtitle mb-2 text-muted">Wild wild west</h6>
-                    <p class="card-text">Roin id est eget massa lacinia condimentum ac hendrerit ante.</p>
-                    <a href="{{ url('/net/jungle') }}" class="card-link">Visit</a>
-                </div>
-            </div>
             <div class="card w-100 m-2">
                 <div class="card-body">
                     <h5 class="card-title">Community</h5>
                     <h6 class="card-subtitle mb-2 text-muted">This is the comunity net.</h6>
                     <p class="card-text">Here everyone is welcome to try out EOS!</p>
                     <a href="{{ url('/net/comunity') }}" class="card-link">Visit</a>
+                </div>
+            </div>
+            <div class="card w-100 m-2">
+                <div class="card-body">
+                    <h5 class="card-title">Jungle</h5>
+                    <h6 class="card-subtitle mb-2 text-muted">Wild wild west</h6>
+                    <p class="card-text">Roin id est eget massa lacinia condimentum ac hendrerit ante.</p>
+                    <a href="{{ url('/net/jungle') }}" class="card-link">Visit</a>
                 </div>
             </div>
             <div class="card w-100 m-2">
