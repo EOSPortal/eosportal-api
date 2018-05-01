c283f947eb5b5a4d32abc1589324864fee03bf29
Henrik Hautakoski <henrik.hautakoski@gmail.com>
2018-05-01 21:38:07 +0200

    app/Http/Controllers/FrontPageController.php: in producerList() fetch "created_at"

--- app/Http/Controllers/FrontPageController.php
+++ app/Http/Controllers/FrontPageController.php
@@ -9,7 +9,8 @@ class FrontPageController extends Controller
     public function producerList()
     {
         $producers = DB::table('producers')
-            ->select(['name', 'organization', 'contact', DB::raw('max(blocks.height) as num_blocks')])
+            ->select(['name', 'organization', 'contact',
+                DB::raw('max(blocks.height) as num_blocks'), DB::raw('max(blocks.created_at) as created_at')])
             ->join('blocks', 'producers.id', '=', 'blocks.producer_id')
             ->groupBy('producers.id')
             ->orderBy('num_blocks', 'DESC')
