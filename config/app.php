9733acafe91b50dae70c43d2446976032e12b6a4
zaratustra <zrts@protonmail.com>
2018-05-01 07:15:12 -0300

    add yajra/laravel-datatables-oracle dependency

--- config/app.php
+++ config/app.php
@@ -150,6 +150,7 @@ return [
         /*
          * Package Service Providers...
          */
+        Yajra\DataTables\DataTablesServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -208,6 +209,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+
+        'DataTables' => Yajra\DataTables\Facades\DataTables::class,
 
     ],
 
