fce52afbd0c4c8dfdb73a1a4c9ccfe0b1f478746
zaratustra <zrts@protonmail.com>
2018-05-03 22:56:09 -0300

    add relationships to models

--- app/Block.php
+++ app/Block.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Block extends Model
 {
-    //
+    public function producer()
+    {
+        return $this->belongsTo('App\Producer');
+    }
+
 }
--- app/Producer.php
+++ app/Producer.php
@@ -6,5 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class Producer extends Model
 {
-    //
+    public function blocks()
+    {
+        return $this->hasMany('App\Block');
+    }
+
+    public function chain()
+    {
+        return $this->belongsTo('App\Chain');
+    }
+
+    public function infos()
+    {
+        return $this->hasMany('App\Info');
+    }
+
 }
