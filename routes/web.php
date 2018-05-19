95dc896a46f6c99de012f8ea28b1c751086cc44a
Ricardo Marliere <rbmarliere@gmail.com>
2018-05-19 11:16:32 -0300

    remote api/ prefix from routes

--- routes/web.php
+++ routes/web.php
@@ -11,11 +11,11 @@
 |
 */
 
-$router->get('api/chains',                          ['uses'=>'ChainController@index']);
-$router->get('api/chains/{chain_id}/infos',         ['uses'=>'InfoController@index']);
-$router->get('api/chains/{chain_id}/blocks',        ['uses'=>'BlockController@index']);
-$router->get('api/chains/{chain_id}/producers',     ['uses'=>'ProducerController@index']);
-$router->get('api/blocks/{block_id}',               ['uses'=>'BlockController@show']);
-$router->get('api/producers/{producer_id}',         ['uses'=>'ProducerController@show']);
-$router->get('api/producers/{producer_id}/blocks',  ['uses'=>'ProducerController@blocks']);
+$router->get('chains',                          ['uses'=>'ChainController@index']);
+$router->get('chains/{chain_id}/infos',         ['uses'=>'InfoController@index']);
+$router->get('chains/{chain_id}/blocks',        ['uses'=>'BlockController@index']);
+$router->get('chains/{chain_id}/producers',     ['uses'=>'ProducerController@index']);
+$router->get('blocks/{block_id}',               ['uses'=>'BlockController@show']);
+$router->get('producers/{producer_id}',         ['uses'=>'ProducerController@show']);
+$router->get('producers/{producer_id}/blocks',  ['uses'=>'ProducerController@blocks']);
 
