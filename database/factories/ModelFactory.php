847717bb926aaa39388ceedd2e746ea809530ad9
Ricardo Marliere <rbmarliere@gmail.com>
2018-05-20 11:28:11 -0300

    add model factories

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -17,3 +17,38 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
         'email' => $faker->email,
     ];
 });
+
+$factory->define(App\Chain::class, function (Faker\Generator $faker) {
+    return [
+        'name' => $faker->lastName,
+        'api_url' => "http://ctestnet.edenx.io:8888",
+    ];
+});
+
+$factory->define(App\Producer::class, function (Faker\Generator $faker) {
+    return [
+        'name' => $faker->name,
+        'organization' => $faker->company,
+        'contact' => $faker->email,
+        'p2p_url' => $faker->ipv4.':9876',
+        'api_url' => 'http://'.$faker->ipv4.':8888',
+    ];
+});
+
+$factory->define(App\Block::class, function (Faker\Generator $faker) {
+    static $n = 0;
+    return [
+        'height' => ++$n,
+        'produced_at' => Carbon\Carbon::now(),
+    ];
+});
+
+$factory->define(App\Info::class, function (Faker\Generator $faker) {
+    static $n = 0;
+    return [
+        'server_version' => '9be89106',
+        'last_irreversible_block_num' => ($n-3)>0?$n-3:0,
+        'head_block_num' => $n++,
+    ];
+});
+
