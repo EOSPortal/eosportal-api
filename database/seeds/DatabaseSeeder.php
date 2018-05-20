a244c9c910132ce863a6a9987769599238a797d6
Ricardo Marliere <rbmarliere@gmail.com>
2018-05-20 11:28:23 -0300

    add default db seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Block;
+use App\Chain;
+use App\Info;
+use App\Producer;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -11,6 +15,19 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call('UsersTableSeeder');
+        $chain = factory(Chain::class)->create();
+        for($p=1;$p<22;$p++) {
+            $producer = factory(App\Producer::class)->make();
+            $chain->producers()->save($producer);
+            for($b=1;$b<13;$b++) {
+                $block = factory(App\Block::class)->make();
+                $producer->blocks()->save($block);
+            }
+            for($i=1;$i<13;$i++) {
+                $info = factory(App\Info::class)->make();
+                $info->head_producer_id = $producer->id;
+                $producer->infos()->save($info);
+            }
+        }
     }
 }
