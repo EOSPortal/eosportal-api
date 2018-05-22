f0deeda0b87466b13761b24570cb562addd1eb63
Cesar Rodriguez <kesarr@gmail.com>
2018-05-22 20:46:23 +0200

    storing chain

--- app/Http/Controllers/ChainController.php
+++ app/Http/Controllers/ChainController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Chain;
 use App\Http\Resources\Chain as ChainResource;
+use App\Chain as ChainModel;
+use Illuminate\Http\Request;
 
 class ChainController extends Controller
 {
@@ -12,4 +14,11 @@ class ChainController extends Controller
         return new ChainResource(Chain::paginate($this->pageSize));
     }
 
+    public function store(Request $request)
+    {
+        $url = $request->input('chain_url');
+        $chain = new ChainModel();
+        $chain->api_url = $url;
+        $chain->save();
+    }
 }
